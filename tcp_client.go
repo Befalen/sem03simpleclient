a84cb1adf3d56610cd09de42754eccc1041c6150
Befalen <joakimnormann@yahoo.com>
2023-04-26 18:01:49 +0000

    endelig endring

--- tcp_client.go
+++ tcp_client.go
@@ -8,28 +8,27 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:8080")
+	conn, err := net.Dial("tcp", "172.17.0.2:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	   log.Println("os.Args[1] = ", os.Args[1])
 
-	log.Println(os.Args[1])
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-	log.Println("Kryptert melding: ", string(kryptertMelding))
-	_, err = conn.Write([]byte(string(kryptertMelding)))
-        if err != nil {
-                log.Fatal(err)
-        }
-        buf := make([]byte, 1024)
-        n, err := conn.Read(buf)
-        if err != nil {
-                log.Fatal(err)
-        }
-	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+	log.Println("os.Args[1] = ", os.Args[1])
 
-        response := string(buf[:n])
-        log.Printf("reply from proxy: %s", response)
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4) //Dette kryptere meldingen
+
+	_, err = conn.Write([]byte(string(kryptertMelding))) //Dette sender den krypterte meldingen
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := string(buf[:n])                                                                                //Dette gjør om fra byte til string
+	log.Printf("reply from proxy: %s", response)                                                               //Printer response
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4) //Meldingen som komme tilbake blir dekrypt her
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))                                              //Printer den dekrypte meldingen
+
 }
